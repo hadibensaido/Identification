bdb5c654ff36edbd0ab0da032dcfd0a527723679
khemis <khemis@PUMA>
2016-07-18 15:03:53 +0200

    Ajoutez des fichiers projet.

---
+++ Identification/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Identification.MvcApplication" Language="C#" %>
---
+++ Identification/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identification.Startup))]
+namespace Identification
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
